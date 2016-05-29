0ca182ea4c6de636a5ccb0e93edc15664e766dab
Kunal Ganglani <kunalganglani@gmail.com>
2016-05-29 18:41:57 +0530

    Dynamic Form

--- app/src/main/java/kunalganglani/com/suveyji/AddForm.java
+++ app/src/main/java/kunalganglani/com/suveyji/AddForm.java
@@ -1,19 +1,47 @@
 package kunalganglani.com.suveyji;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AddForm extends AppCompatActivity {
-    public void onBackPressed() {
-        super.onBackPressed();
-        overridePendingTransition(R.anim.left_in, R.anim.right_out);
-    }
 
+    private LinearLayout mLayout;
+    private EditText mEditText;
+    private Button mButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_form);
-        // Comment Added
+        mLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(onClick());
+        TextView textView = new TextView(this);
+        textView.setText("New text");
+    }
+
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mLayout.addView(createNewTextView(mEditText.getText().toString()));
+            }
+        };
+    }
+
+    private TextView createNewTextView(String text) {
+        final ActionBar.LayoutParams lparams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setText("New text: " + text);
+        return textView;
     }
 }
